98dc67797938dedd1106f9ca80c025eb81398a38
Kchao1910 <kchao1910@gmail.com>
2019-03-21 22:04:46 -0700

    UI refinement

--- bj-testing/DisplayedCard.js
+++ bj-testing/DisplayedCard.js
@@ -7,13 +7,13 @@ function DisplayedCard(divid, card) {
    this.card = card || new Card(Card.ACE, Card.SPADE);
    this.faceDown = true;
    this.div = document.getElementById(divid);
-   this.div.style.width = "79px";
-   this.div.style.height = "123px";
+   this.div.style.width = "154.88px";
+   this.div.style.height = "230.31px";
    var div1 = document.createElement("div");
    this.cardContainer = div1;
    div1.style.position = "relative";
-   div1.style.width = "79px";
-   div1.style.height = "123px";
+   div1.style.width = "154.88px";
+   div1.style.height = "230.31px";
    div1.style.left = "0px";
    div1.style.top = "0px";
    this.div.appendChild(div1);
@@ -21,34 +21,34 @@ function DisplayedCard(divid, card) {
    this.cardImg = cardImg;
    cardImg.src = "cards.png";
    cardImg.style.position = "absolute";
-   cardImg.style.width = (79*13) + "px";
-   cardImg.style.height = (123*5) + "px";
+   cardImg.style.width = (154.88 * 13) + "px";
+   cardImg.style.height = (230.31 * 5) + "px";
    this.setPositionAndClip();
    div1.appendChild(cardImg);
 }
 
-DisplayedCard.prototype.setPositionAndClip = function() {
+DisplayedCard.prototype.setPositionAndClip = function () {
    var left, top;
    if (this.faceDown) {
-      left = 2*79;
-      top = 4*123;
+      left = 2 * 154.88;
+      top = 4 * 230.31;
    }
    else {
-      left = (this.card.value-1) * 79;
-      top = (this.card.suit-1) * 123;
+      left = (this.card.value - 1) * 154.88;
+      top = (this.card.suit - 1) * 230.31;
    }
    this.cardImg.style.left = "-" + left + "px";
    this.cardImg.style.top = "-" + top + "px";
-   this.cardImg.style.clip = "rect(" + top + "px " + (left+79) + "px "  + 
-                                      (top+123) + "px "  + left + "px)";
+   this.cardImg.style.clip = "rect(" + top + "px " + (left + 154.88) + "px " +
+      (top + 230.31) + "px " + left + "px)";
 }
 
-DisplayedCard.prototype.setCard = function(card) {
+DisplayedCard.prototype.setCard = function (card) {
    this.card = card;
    this.setPositionAndClip();
 }
 
-DisplayedCard.prototype.setFaceDown = function() {
+DisplayedCard.prototype.setFaceDown = function () {
    if (arguments.length == 0)
       this.faceDown = true;
    else
@@ -56,11 +56,11 @@ DisplayedCard.prototype.setFaceDown = function() {
    this.setPositionAndClip();
 }
 
-DisplayedCard.prototype.setFaceUp = function() {
+DisplayedCard.prototype.setFaceUp = function () {
    if (arguments.length == 0)
       this.faceDown = false;
    else
-      this.faceDown = ! Boolean(arguments[0]);
+      this.faceDown = !Boolean(arguments[0]);
    this.setPositionAndClip();
 }
 
